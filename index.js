54c828678c4ab5408b151ca4b4f6f77a92257278
vu truong <vu2551984@gmail.com>
2017-11-07 08:29:01 +0700

    7-11-1

--- index.js
+++ index.js
@@ -194,9 +194,8 @@ io.on('connection',  (socket)=>
 			else{
         //Nếu đăng nhập đúng
       //  socket.emit('okfirstlogin',{number:rows[0].number, pass:rows[0].pass, name:rows[0].user});
-      socket.emit('dangnhap_dung', {number:rows[0].number, pass:rows[0].pass, name:"vu truong"});
-    //var doituong = {name:rows[0].user};
-    //  socket.emit('dangnhap_dung',(date)=>{console.log('Da gui su kien di'+date);});
+      socket.emit('dangnhap_dung', {number:rows[0].number, pass:rows[0].pass, name:rows[0].user});
+
         console.log('Dang nhap dung roi voi ten:'+rows[0].user);
         // bắt đầu kiểm tra các thông tin cần gửi cho user này đang được lưu trên db
         // đầu tiên là các room gửi cho user
