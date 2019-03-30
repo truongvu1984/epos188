dd4d972f6ccc6775ed1f0de0abf768bc513624a2
vu truong <vu2551984@gmail.com>
2019-03-30 10:41:35 +0700

    30-3

--- index.js
+++ index.js
@@ -1153,7 +1153,7 @@ io.on('connection',(socket)=>
               val2 = [[ res.insertId, member.name,member.number,'B']];
               con.query(sql2, [val2], function (err2, res2){if ( err2){console.log(err2);}});
               });
-              io.sockets.in(socket.number)('S_send_room',{ids:res.insertId,room_name:strencode(info.room_name), room_id_server:room_id, admin_name:strencode(socket.username), admin_number:socket.number, time:get_time(thoigian)});
+              io.sockets.in(socket.number).emit('S_send_room',{ids:res.insertId,room_name:strencode(info.room_name), room_id_server:room_id, admin_name:strencode(socket.username), admin_number:socket.number, time:get_time(thoigian)});
             }
           });
         }
