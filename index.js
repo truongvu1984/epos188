b38641c34407645a220b26e15b53e91664873133
vu truong <vu2551984@gmail.com>
2019-06-24 09:12:47 +0700

    24-6

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ con.connect(function(err) {
         else {
           let sess = req.session;
           console.log(sess);
+          console.log(sess.ok);
+
           var full_number = "+"+req.body.code + req.body.number.replace('0','');
           con.query("SELECT * FROM `account` WHERE `number` LIKE '"+full_number+"' LIMIT 1", function(err, rows){
             if (err || rows.length ==0){res.render('dangnhap3', {noidung:'Tài khoản này không tồn tại'});}
