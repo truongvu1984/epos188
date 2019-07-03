e9bdcaf888871ab2ada09dacbf0c4a6a862074e7
vu truong <vu2551984@gmail.com>
2019-07-03 21:54:50 +0700

    3-7

--- index.js
+++ index.js
@@ -315,6 +315,7 @@ io.on('connection',(socket)=>
 // return rightuser&&right_pass&&online&&inbox&&send&&save&&contact&&group
 //   }
   socket.on('login2',(data)=>{
+    console.log(check_data1(data.online));
     if(data.rightuser&&data.right_pass&&check_data1(data.online)&&check_data1(data.inbox)&&check_data1(data.send)&&check_data1(data.save)&&check_data1(data.contact)&&check_data1(data.group)){
       con.query("SELECT * FROM `account` WHERE `number` LIKE '"+data.rightuser+"' LIMIT 1", function(err, rows){
   	    if (err || rows.length ==0){socket.emit('login2_khongtaikhoan');}
