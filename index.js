55f5bd07facd27f13d16d6232dd19b4ae831b393
vu truong <vu2551984@gmail.com>
2019-12-13 10:46:07 +0700

    13-12

--- index.js
+++ index.js
@@ -693,19 +693,19 @@ io.on('connection',(socket)=>
                 }
               });
             // // Lấy danh sách room
-            con.query("SELECT * FROM `"+socket.number+"mes_main`  WHERE `send_receive` LIKE 'O' AND `id` > "+data.online+" ORDER BY `id` ASC", function(err1, a1s){
-                if (err1){console.log('Da co loi room full:'+err1);}
-                else if(a1s.length>0)
-                  {
-                     a1s.forEach(function(a1,key){
-                      con.query("SELECT * FROM `"+socket.number+"mes_sender` WHERE `ids` LIKE '"+a1.id+"' AND `send_receive` LIKE 'A' LIMIT 1 ", function(err5, a5s)
-                        {
-                          if ( err5 ){console.log(err5);}
-                          else  {if(a5s.length>0){socket.emit('S_send_room',{ids:a1.id,room_name:strencode(a1.subject), room_id_server:a1.idc, admin_name:strencode(a5s[0].name), admin_number:a5s[0].number, time:get_time(a1.time), stt:a1.stt});}}
-                        });
-                    });
-                }
-            });
+            // con.query("SELECT * FROM `"+socket.number+"mes_main`  WHERE `send_receive` LIKE 'O' AND `id` > "+data.online+" ORDER BY `id` ASC", function(err1, a1s){
+            //     if (err1){console.log('Da co loi room full:'+err1);}
+            //     else if(a1s.length>0)
+            //       {
+            //          a1s.forEach(function(a1,key){
+            //           con.query("SELECT * FROM `"+socket.number+"mes_sender` WHERE `ids` LIKE '"+a1.id+"' AND `send_receive` LIKE 'A' LIMIT 1 ", function(err5, a5s)
+            //             {
+            //               if ( err5 ){console.log(err5);}
+            //               else  {if(a5s.length>0){socket.emit('S_send_room',{ids:a1.id,room_name:strencode(a1.subject), room_id_server:a1.idc, admin_name:strencode(a5s[0].name), admin_number:a5s[0].number, time:get_time(a1.time), stt:a1.stt});}}
+            //             });
+            //         });
+            //     }
+            // });
 
             }
           else {socket.emit('login2_sai');}
