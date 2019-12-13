5b806925e6277059b6b2fa85594a7e27616bb097
vu truong <vu2551984@gmail.com>
2019-12-13 09:32:18 +0700

    13-12

--- index.js
+++ index.js
@@ -594,7 +594,7 @@ io.on('connection',(socket)=>
   socket.on('login2',(data)=>{
   abc++;
   console.log('Có nhận yêu cầu 2:'+data.inbox);
-    // if(data.rightuser&&data.right_pass&&check_data1(data.online)&&check_data1(data.inbox)&&check_data1(data.send)&&check_data1(data.save)&&check_data1(data.contact)&&check_data1(data.group)){
+    if(data.rightuser&&data.right_pass&&check_data1(data.online)&&check_data1(data.inbox)&&check_data1(data.send)&&check_data1(data.save)&&check_data1(data.contact)&&check_data1(data.group)){
       con.query("SELECT * FROM `account` WHERE `number` LIKE '"+data.rightuser+"' LIMIT 1", function(err, rows){
   	    if (err || rows.length ==0){socket.emit('login2_khongtaikhoan');}
         else{
@@ -717,9 +717,9 @@ io.on('connection',(socket)=>
           else {socket.emit('login2_sai');}
         }
      	 });
-     // }
+     }
 	});
-  
+
   socket.on('C_del_inbox',(mes)=>{
     if(socket.number&&isArray(mes)){
       mes.forEach((mes1,key)=>{
