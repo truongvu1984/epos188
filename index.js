fc5241f0e77b5bcd27f157741bbd2f807ec7da02
vu truong <vu2551984@gmail.com>
2019-12-13 09:49:10 +0700

    13-12

--- index.js
+++ index.js
@@ -612,12 +612,10 @@ io.on('connection',(socket)=>
                   a1s.forEach((a1,key)=>{
                     socket.emit('lilulu');
                     con.query("SELECT `id`,`name`, `number` FROM `"+socket.number+"mes_sender` WHERE `ids` LIKE '"+a1.id+"' LIMIT 1", function(err2, a2s){
-
-                     // con.query("SELECT * FROM `"+socket.number+"mes_main`", function(err2, a2s){
                        if(err2){console.log(err2);}
                        else {
-                         socket.emit('lalala');
-                        // socket.emit('kaka',{ids:a1.id,name_nguoigui:strencode(a2s[0].name),number_nguoigui:a2s[0].number, subject:strencode(a1.subject), id_tinnha_client:a1.idc,read_1:a1.read_1, stt: a1.stt,time:get_time(a1.time)});
+                         socket.emit('huhuhu');
+                         // socket.emit('kaka',{ids:a1.id,name_nguoigui:strencode(a2s[0].name),number_nguoigui:a2s[0].number, subject:strencode(a1.subject), id_tinnha_client:a1.idc,read_1:a1.read_1, stt: a1.stt,time:get_time(a1.time)});
                          console.log('có gửi inbox đi');
                        }
                      });
