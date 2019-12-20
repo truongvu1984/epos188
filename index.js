90131f73fa86325f1df1455e5a571ef3ec66c5d7
vu truong <vu2551984@gmail.com>
2019-12-20 10:15:44 +0700

    20-12

--- index.js
+++ index.js
@@ -630,9 +630,9 @@ io.on('connection',(socket)=>
                             else {
                               a2s.forEach(function(a2,key2){
                                 nhomnguoinhan.push({number:a2.number, name:strencode(a2.name),stt:a2.stt});
-                                // if(key2 === (a2s.length-1)){
-                                //   socket.emit('S_send_send',{ids:a1.id,subject:strencode(a1.subject), idc:a1.idc,time:get_time(a1.time), nguoinhan:nhomnguoinhan});
-                                // }
+                                if(key2 === (a2s.length-1)){
+                                  socket.emit('S_send_send',{ids:a1.id,subject:strencode(a1.subject), idc:a1.idc,time:get_time(a1.time), nguoinhan:nhomnguoinhan});
+                                }
                               });
                             }
                           });
