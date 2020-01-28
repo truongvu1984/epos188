1518e962716b6e064f8efd3ef7a7dc9b4ec4b7c3
vu truong <vu2551984@gmail.com>
2020-01-28 18:38:26 +0700

    28-1

--- index.js
+++ index.js
@@ -743,20 +743,20 @@ io.on('connection',(socket)=>
         con.query("SELECT * FROM `"+socket.number+"mes_main` WHERE `send_receive` LIKE 'S' AND `idc` LIKE '"+tin.idc+"' LIMIT 1", function(err1, a1s){
           if(err1){console.log(err1);}
           else {
-            con.query("SELECT * FROM `"+socket.number+"mes_sender` WHERE `send_receive` LIKE 'S' AND `ids` LIKE '"+a1s[0].id+"' AND `stt` LIKE 'Y'", function(err2, a2s){
-              if(err2){console.log(err2);}
-              else {
-                if(a2s.length > tin.number){
-                  a2s.forEach((a2,key2)=>{
-                    list.push({number:a2.number, name:strencode(a2.name),stt:a2.app});
-                    if(key2===a2s.length){
-                      list_full.push({list:list,idc:tin.idc});
-                      if(key===data.length){socket.emit('S_check_send',list_full);}
-                    }
-                  });
-                }
-              }
-            });
+            // con.query("SELECT * FROM `"+socket.number+"mes_sender` WHERE `send_receive` LIKE 'S' AND `ids` LIKE '"+a1s[0].id+"' AND `stt` LIKE 'Y'", function(err2, a2s){
+            //   if(err2){console.log(err2);}
+            //   else {
+            //     if(a2s.length > tin.number){
+            //       a2s.forEach((a2,key2)=>{
+            //         list.push({number:a2.number, name:strencode(a2.name),stt:a2.app});
+            //         if(key2===a2s.length){
+            //           list_full.push({list:list,idc:tin.idc});
+            //           if(key===data.length){socket.emit('S_check_send',list_full);}
+            //         }
+            //       });
+            //     }
+            //   }
+            // });
           }
         });
       }
