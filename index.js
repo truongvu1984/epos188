d04538fa2881abcd2466608c7447ce76f084eff3
vu truong <vu2551984@gmail.com>
2020-09-19 20:28:22 +0700

    19-9

--- index.js
+++ index.js
@@ -617,9 +617,9 @@ io.on('connection',(socket)=>
     return abc;
   }
   socket.on('C_send_alarm',(data)=>{
-
-  if(socket.number){
     console.log(data);
+  if(socket.number){
+
     if(data.name != null&&data.ma != null&&data.type != null&&data.lat != null&&data.lon != null&&data.culy != null&&data.ring != null&&data.uri != null){
       let thoigian = new Date();
       console.log('Có nhận alarm hi hi');
