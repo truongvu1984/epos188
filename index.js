6029800549892fa73eed316d55fb7ffde6d080a4
vu truong <vu2551984@gmail.com>
2020-10-09 14:45:52 +0700

    9-10

--- index.js
+++ index.js
@@ -648,16 +648,19 @@ io.on('connection',(socket)=>
   });
   socket.on('C_reg_new_online',(num)=>{
     if(socket.number&&num){
+      console.log(num);
       if(num==0){
         con.query("SELECT * FROM `"+socket.number+"mes_main`  WHERE `send_receive` LIKE 'O' ORDER BY `id` DESC LIMIT 20", function(err1, a1s){
           if (err1){console.log('Da co loi room full:'+err1);}
           else if(a1s.length>0)
             {
+              console.log('so luong la:'+a1s.length);
                a1s.forEach(function(a1,key){
                 con.query("SELECT `name`,`number` FROM `"+socket.number+"mes_sender` WHERE `ids` LIKE '"+a1.id+"' AND `send_receive` LIKE 'A' LIMIT 1 ", function(err5, a5s)
                   {
                     if ( err5 ){console.log(err5);}
                     else  {if(a5s.length>0){
+
                         socket.emit('S_send_new_room',{ids:a1.id,room_name:strencode(a1.subject), room_id_server:a1.idc, admin_name:strencode(a5s[0].name), admin_number:a5s[0].number, time:get_time(a1.time), stt:a1.stt,abc:'A'});
 
                     }
