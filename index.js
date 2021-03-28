0c95475366665c335e525748f44f1c86acfe8122
vutruong <vu2551984@gmail.com>
2021-03-28 22:33:40 +0700

    29-3

--- index.js
+++ index.js
@@ -585,7 +585,7 @@ io.on('connection',(socket)=>
     }
   });
   socket.on('C_reg_friend',(ids,num)=>{
-    if(socket.number&&name1!=null&&num!=null&&(!isNaN(num))){
+    if(socket.number&&ids!=null&&num!=null&&(!isNaN(num))){
         if(num==0){
 
         con.query("SELECT * FROM `"+socket.number+"contact` ORDER BY `id` ASC LIMIT 50", function(err1, a1s)
