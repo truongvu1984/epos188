5140daa137c2aa9888ee9fe2450ed97e38641df8
vutruong <vu2551984@gmail.com>
2021-06-01 09:28:48 +0700

    1-6

--- index.js
+++ index.js
@@ -504,17 +504,17 @@ io.on('connection',(socket)=>
     }
     else socket.emit('check_pass');
   });
-  // socket.on('reg_old_game',(mail)=>{
-  //   if(socket.number != null){
-  //     if(mail!= null) io.sockets.in(mail).emit('C_reg_old_game',socket.number);
-  // }
-  // else socket.emit('check_pass');
-  // });
+  socket.on('C_reg_old_game',(mail)=>{
+    if(socket.number != null){
+      if(mail!= null) io.sockets.in(mail).emit('S_reg_old_game',socket.number);
+  }
+  else socket.emit('check_pass');
+  });
   socket.on('C_send_old_game',(mail,ten,ban,ta,luot)=>{
     if(socket.number != null){
         if(mail!= null &&ban!= null&&ta!= null &&ten !=null && luot != null){
 
-       io.sockets.in(mail).emit('C_send_old_game_2',{mail:socket.number,name:strencode(ten),toado_ban:ban,toado_ta:ta, luotchoi:luot});
+       io.sockets.in(mail).emit('S_send_old_game',{mail:socket.number,name:strencode(ten),toado_ban:ban,toado_ta:ta, luotchoi:luot});
 
     }
   }
