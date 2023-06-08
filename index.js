c2814f8d7e6e75b3e57386af3c3d0c23ebbbf9d7
truongvu1984 <vu2551984@gmail.com>
2023-06-08 07:43:22 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -58,9 +58,9 @@ con.connect(function(err) {
     if (err) { console.log(" da co loi:" + err);}
     else {
       let time=new Date();
-      var sql = "INSERT INTO `test`(abc) VALUES ?";
-        var values = [time,time];
-        con.query(sql, [values], function (err1, result) {
+      // var sql = "INSERT INTO `test`(abc) VALUES ?";
+      //   var values = [time,time];
+        con.query("INSERT INTO `test`(abcd) VALUES (time)", function (err1, result) {
           if(err1)console.log(err1);
           else {
             con.query("SELECT * FROM `test` WHERE `id` = '1' LIMIT 1", function(err, rows){
