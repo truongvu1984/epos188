42d0e2b31788a73bbd15d5f4d57d66401c0e72dc
truongvu1984 <vu2551984@gmail.com>
2023-08-16 14:54:57 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2330,11 +2330,11 @@ con.connect(function(err) {
           if (err1)socket.emit("giao_nhiemvu_thatbai","L");
           else {
               socket.emit('gui_thongtin_ok',{nd:'L',idc:idc,tt:result.insertId});
-              io.sockets.in("chung").emit("S_capnhat_vitri",{lat:nd2.lat,lon:nd2.lon,name:nd2.name,diadanh:nd2.diadanh,idc:idc,tt:result.insertId});
+              io.sockets.in("chung").emit("S_capnhat_vitri",{lat:nd2.lat,lon:nd2.lon,name:nd2.name,diadanh:nd2.diadanh,idc:idc,tt:result.insertId,hinhanh: nd2.hinhanh,hinhanh_tt:0});
               if(socket.type=="F"){
                   con.query("SELECT `ch1_user` FROM `list_err` WHERE `idc` LIKE '"+idc+"' LIMIT 1", function(err, rows){
                       if (err){console.log(err);}
-                      else io.sockets.in(rows[0].ch1_user).emit("S_capnhat_vitri",{lat:nd2.lat,lon:nd2.lon,name:nd2.name,diadanh:nd2.diadanh,idc:idc,tt:result.insertId});
+                      else io.sockets.in(rows[0].ch1_user).emit("S_capnhat_vitri",{lat:nd2.lat,lon:nd2.lon,name:nd2.name,diadanh:nd2.diadanh,idc:idc,tt:result.insertId,hinhanh: nd2.hinhanh,hinhanh_tt:0});
                   });
               }
             }
