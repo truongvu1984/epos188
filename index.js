efb327ab300524681c1c24f626e61cf90d0b5d76
truongvu1984 <vu2551984@gmail.com>
2024-09-03 09:18:34 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1000,23 +1000,24 @@ con.connect((err)=> {
                       let idc=row.idc;
                       let list_line=[];
                       let list_diem=[];
-                      con.query("SELECT * FROM `"+socket.number+"line_main` WHERE `idc` LIKE '"+idc+"'", function(err1, row1s){
+                      con.query("SELECT * FROM `"+socket.number+"line_main` WHERE `idc` LIKE '"+idc+"'", (err1, row1s)=>{
                         if (err1){socket.emit('login2_khongtaikhoan');}
                         else if(row1s.length>0){
                           console.log('AAAA'+row1s.length);
                           row1s.forEach((row1, i1) => {
-                            con.query("SELECT * FROM `"+socket.number+"line_detail` WHERE `idc` LIKE '"+row1.idlo+"'", function(err2, row2s){
+                            con.query("SELECT * FROM `"+socket.number+"line_detail` WHERE `idc` LIKE '"+row1.idlo+"'", (err2, row2s)=>{
                               if (err2){socket.emit('login2_khongtaikhoan');}
                               else if(row2s.length>0){
                                   console.log('BBBBB'+row2s.length);
                                 list_line.push({name:row1.name,culy:row1.culy,up_id:idc,list_line1:row2s,idlo:row1.idlo});
                                 if(i1===(row1s.length-1)){
-                                  con.query("SELECT * FROM `"+socket.number+"diem` WHERE `idc` LIKE '"+idc+"'", function(err3, row3s){
+                                  con.query("SELECT * FROM `"+socket.number+"diem` WHERE `idc` LIKE '"+idc+"'", (err3, row3s)=>{
                                     if(err3){socket.emit('login2_khongtaikhoan');}
-                                    else if(row3s.length>0){
+                                    else {
                                       socket.emit('S_send_tinnhan',{name_nguoigui:row.name,number_nguoigui:row.number,
                                                                 subject: row.subject, idc:row.idc, time:get_time(row.time),list_line:list_line,list_diem:row3s});
                                     }
+
 
                                   });
                                 }
